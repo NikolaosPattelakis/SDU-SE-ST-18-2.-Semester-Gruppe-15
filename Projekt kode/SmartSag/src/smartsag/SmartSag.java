7e37ef21873be3a610336c3af2c0ceef30107913
Unknown <lomedil.np@gmail.com>
2019-05-04 11:14:36 +0200

    Beginning to clean up and adjust
    Cleaned main up, added packages and classes for MVC pattern.

--- Projekt kode/SmartSag/src/smartsag/SmartSag.java
+++ Projekt kode/SmartSag/src/smartsag/SmartSag.java
@@ -1,22 +1,11 @@
 package smartsag;
 
-import Persistence.DataHandler;
-import Presentation.CommandInterface;
+import Controller.Controller;
+import Model.Model;
+import View.View;
 
 
 public class SmartSag {
-
-    public final static String ROLES_XML_FILEPATH = "data/roles.xml";
-    public final static String CASES_XML_FILEPATH = "data/cases.xml";
-
-    public static int CURRENT_ROLE_ID;
-    public static Role currentRole;
-
-    public static DataHandler dataHandlerRole;
-    public static DataHandler dataHandlerCase;
-
-    public static RoleHandler roleHandler;
-    public static CaseHandler caseHandler;
 
     /**
      * Main class <br>
@@ -27,47 +16,12 @@ public class SmartSag {
      * @param args
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
 
-        dataHandlerRole = new DataHandler(ROLES_XML_FILEPATH);
+        View view = new View();
+        Model model = new Model();
+        Controller controller = new Controller(view, model);
         
-        //Example of command line interface with case handling
-        //User is Administrator
-        //User can edit and delete
-        //User cannot create and read
-        CommandInterface cli = new CommandInterface();
-        cli.run();
-        
-        //Examples of methods regarding role handling
-        
-        setUserToKoordinator();
-        
-        setUserToAdministrator();
-        
-        roleHandler = new RoleHandler(CURRENT_ROLE_ID);
-        
-        createRole();
-        
-        deleteRole();
     }
     
-    private static void setUserToKoordinator(){
-        CURRENT_ROLE_ID = 1;
-    }
-    
-    private static void setUserToAdministrator(){
-        CURRENT_ROLE_ID = 0;
-    }
-    
-    private static void createRole(){
-        Role newRole = new Role();
-        newRole.setName("Vikar");
-        newRole.setName("Lumby");
-        roleHandler.createRole(newRole);
-    }
- 
-    private static void deleteRole(){
-        int roleID = 2;
-        roleHandler.deleteRole(roleID);
-    }
 }
